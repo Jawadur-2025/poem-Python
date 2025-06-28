cee6831b302e391b7dec851fdc254d2ce36b838c
Md. Jawadur Rahman Chowdhury <jawadurrahmanmahdi@gmail.com>
2025-06-28 15:55:18 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+print("Hello World")
+print("Now I am going to write a poem in python")
+# this is my first programme. I hope everybody will like it.
+# Define the lines of the poem
+poem_lines = [
+    "Twinkle, twinkle, little star,",
+    "How I wonder what you are.",
+    "Up above the world so high,",
+    "Like a diamond in the sky.",
+    "Twinkle, twinkle, little star,",
+    "How I wonder what you are.",
+    "",
+    "When the blazing sun is set,",
+    "And the grass with dew is wet.",
+    "Then you show your little light,",
+    "Twinkle, twinkle all the night.",
+    "Twinkle, twinkle little star,",
+    "How I wonder what you are.",
+    "",
+    "Then the traveler in the dark",
+    "Thanks you for your tiny spark.",
+    "How could he see where to go",
+    "If you did not twinkle so?",
+    "Twinkle, twinkle little star,",
+    "How I wonder what you are.",
+    "",
+    "As your bright and tiny spark",
+    "Lights the traveler in the dark,",
+    "Though I know not what you are,",
+    "Twinkle, twinkle, little star.",
+    "Twinkle, twinkle, little star,",
+    "How I wonder what you are."
+]
+
+# Print the poem
+for line in poem_lines:
+    print(line)
+
+ # Define the line of the poem
+poem_lines  = [
+     "Risk, by Anaïs Nin"
+     "And then the day came,"
+     "when the risk"
+     "to remain tight"
+     " in a bud"
+     "was more painful"
+     "than the risk"
+     "it took"
+     "to blossom."
+]
+
+# print the poem
+for lines in poem_lines:
+    print(line)
